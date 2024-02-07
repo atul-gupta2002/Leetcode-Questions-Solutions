f5db2ff54cdb58fb346257ca74770e4e358d1559
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-02-07 23:40:35 +0530

    Time: 4 ms (65.41%) | Memory: 48.6 MB (33.95%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.java
+++ 386-lexicographical-numbers/lexicographical-numbers.java
@@ -1,27 +1,23 @@
-class Solution {
+    class Solution {
     public List<Integer> lexicalOrder(int n) {
-        List<Integer> li = new ArrayList<>();
-        printLexicographical(0,n,li);
-        return li;
-
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        lexic(n,0,num);
+        return num;
     }
-        public static void printLexicographical(int current, int n,List<Integer> li) {
-            if (current > n) {
-                return;
-            }
-
-
-            int i=0;
-            if(current ==0){
-                i=1;
-
-            }
-            else{
-                li.add(current);
-            }
-        
-            for (; i <= 9; i++) {
-                printLexicographical(current * 10 + i,n,li);
-            }
+    public static void lexic(int n,int cur,ArrayList<Integer> num){
+        if(cur>n){
+            return;
         }
+        int i=0;
+        
+        if(cur==0){
+        
+            i=1;
+        }
+        else{
+        num.add(cur);}
+        for(;i<=9;i++){
+            lexic(n,cur*10+i,num);
+        }
+    }
 }
