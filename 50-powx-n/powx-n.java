c1ccbc30d02e85b493934e79026d16757fd056d6
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-05-17 20:22:14 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (24.43%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,7 +1,24 @@
 class Solution {
     public double myPow(double x, int n) {
-        double power=Math.pow(x,n);
-        return power;
         
+        if(n < 0){
+            n = -n;
+            x = 1 / x;
+            
+        }
+        
+        double pow = 1;
+        
+        while(n != 0){
+            if((n & 1) != 0){
+                pow *= x;
+            } 
+                
+            x *= x;
+            n >>>= 1;
+            
+        }
+        
+        return pow;
     }
 }
