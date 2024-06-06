0f86ddea531d006d66469e61ffe4d22d5c73bd40
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-06-06 14:15:00 +0530

    Time: 576 ms (7.21%) | Memory: 53 MB (98.51%) - LeetSync

---
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isPossibleDivide(int[] hand, int groupSize) {
+        int n=hand.length;
+        if(n%groupSize!=0){
+            return false;
+        }
+        Arrays.sort(hand);
+        for(int i=0;i<n;i++){
+            if(hand[i] >=0){
+                if(!check(hand,groupSize,i)){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    boolean check(int[] hand, int groupSize,int i){
+        int n=hand.length;
+        int x=hand[i]+1;
+        hand[i]=-1;
+        int count=1;
+        i++;
+        while(count<groupSize && i<n){
+            if(hand[i]== x){
+                x=hand[i]+1; 
+                hand[i]=-1;
+                count++;
+            }
+            i++;
+        }
+        if(count!=groupSize){
+            return false;
+        }
+        else{
+            return true;
+        }
+        
+    }
+}
