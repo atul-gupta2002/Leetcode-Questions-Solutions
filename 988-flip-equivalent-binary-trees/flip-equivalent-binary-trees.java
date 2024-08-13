f882796f3d5d736a0d90325348abb5b3f79c82b6
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-08-13 08:46:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (35.55%) - LeetSync

--- 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
+++ 988-flip-equivalent-binary-trees/flip-equivalent-binary-trees.java
@@ -15,12 +15,12 @@
  */
 class Solution {
     public boolean flipEquiv(TreeNode root1, TreeNode root2) {
-        if(root1 == null){
-            return root2 == null;
-        }
-        if(root2 == null){
-            return root1 == null;
-        }
+         if(root1==null && root2==null){
+            return true;
+         }
+         if(root1==null || root2==null){
+            return false;
+         }
         if(root1.val!=root2.val){
             return false;
         }
