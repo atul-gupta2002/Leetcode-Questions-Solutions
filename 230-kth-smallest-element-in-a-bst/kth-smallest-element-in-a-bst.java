887ca18a86873622e04594fbc1c1e4d9a8354a63
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-09-21 14:26:20 +0530

    Time: 2 ms (11.29%) | Memory: 43.9 MB (93.63%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -14,25 +14,25 @@
  * }
  */
 class Solution {
-    int count=0;
     public int kthSmallest(TreeNode root, int k) {
-        return helper(root,k).val;
+      PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+      helper(root,minHeap,k);
+
+      int ans=0;
+      for(int i=0;i<k;i++){
+        ans=minHeap.poll();
+      }
+      return ans;
     }
-    public TreeNode helper(TreeNode root,int k){
-        
-        if(root==null){
-            return null;
+    public void helper(TreeNode node,PriorityQueue<Integer> minHeap ,int k){
+        if(node==null){
+            return;
         }
-        TreeNode left =helper(root.left,k);
-        if(left!=null){
-            return left;
-        }
-        count++;
-        if(count==k){
-            return root;
-        }
-        return helper(root.right,k);
-        
+        helper(node.left,minHeap,k);
+        minHeap.offer(node.val);
+         helper(node.right,minHeap,k);
+
     }
-    
+
+
 }
