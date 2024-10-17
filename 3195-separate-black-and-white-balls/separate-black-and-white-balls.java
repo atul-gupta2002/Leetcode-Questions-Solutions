235f1fb93cf9d79e5df0193269b0693ae5396cfa
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-10-17 20:41:35 +0530

    Time: 7 ms (99.72%) | Memory: 45 MB (85.46%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long minimumSteps(String s) {
+       long swap=0;
+        long black=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='0'){
+                swap+=black;
+            }
+            else{
+                black++;
+            }
+        }
+        return swap;
+        
+    }
+}
