e24fdad64c732cf06d8104c345586075b5c519bd
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-12-06 23:58:13 +0530

    Time: 4 ms (100.00%) | Memory: 45 MB (100.00%) - LeetSync

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,18 +1,37 @@
 class Solution {
+
     public int maxCount(int[] banned, int n, int maxSum) {
-        Set<Integer> bannedSet = new HashSet<>();
-        for (int num : banned) {
-            bannedSet.add(num);
-        }
-        
-        int sum = 0, count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (!bannedSet.contains(i) && sum + i <= maxSum) { // Skip banned numbers
-                sum += i;
-                count++;
-            }
+
+        // Create a lookup array for banned numbers
+        int[] bannedArr = new int[10001];
+        for (int ban : banned) {
+            bannedArr[ban] = 1; // Mark banned numbers
         }
 
+        // Initialize counters
+        int count = 0;
+        int sum = 0;
+
+        // Iterate through numbers from 1 to n
+        for (int i = 1; i <= n; i++) {
+            
+            // Check if the current number is banned
+            if (bannedArr[i] == 1) 
+            {
+                continue;
+            }
+
+            // Check if adding the current number exceeds maxSum
+            if (sum + i > maxSum) {
+                break;
+            }
+
+            // Add the current number to the selection
+            count++;
+            sum += i;
+        }
+
+        // Return the maximum count of valid numbers
         return count;
     }
 }
