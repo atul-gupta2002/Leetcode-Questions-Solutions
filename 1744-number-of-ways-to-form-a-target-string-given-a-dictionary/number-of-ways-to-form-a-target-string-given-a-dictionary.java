39300667e1ad4f01f75c82672f494f2b7e1195d3
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2024-12-30 00:22:24 +0530

    Time: 18 ms (99.63%) | Memory: 56.1 MB (41.39%) - LeetSync

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.java
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/number-of-ways-to-form-a-target-string-given-a-dictionary.java
@@ -1,46 +1,37 @@
 class Solution {
-    private int MOD = (int) 1e9+7;
+    public static final int MOD=1000000007;
     public int numWays(String[] words, String target) {
-        if(target.length() > words[0].length()){
+        int n = words[0].length();
+        int m = target.length();
+        if (m > n) {
             return 0;
         }
-
-        long[][] dp = new long[target.length()][words[0].length()];
-
-        for(long[] it : dp){
-            Arrays.fill(it,-1);
-        }
-
-        return (int) helper(words, target, 0, 0, dp) % MOD;
-    }
-
-    private long helper(String[] words, String target, int i, int j, long[][] dp){
-        if(i == target.length()){
-            return 1;
-        }
-        if(j == words[0].length()){
-            return 0;
-        }
-
-        int remTarget = target.length() - i;
-        int remWord = words[0].length() - j;
-
-        if(remTarget > remWord){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        long val = helper(words, target, i, j+1, dp);
-
-        for(int k = 0;k < words.length;k++){
-            if(words[k].charAt(j) == target.charAt(i)){
-                val += helper(words, target, i+1, j+1, dp);
+        int[][] freq = new int[n][26];
+        for (String s : words) {
+            for (int i = 0; i < n; i++) {
+                freq[i][s.charAt(i) - 'a']++;
             }
         }
+        return f(n, m, target, freq,new Integer[n+1][m+1]);
+    }
 
-        return dp[i][j] = (val % MOD);
+    int f(int i, int j, String target, int[][] freq,Integer[][] dp) {
+        if( i < j ){
+            return 0;
+        }
+        if (j == 0 ) {
+            return 1;
+        }
+        if(dp[i][j]!=null){
+            return dp[i][j];
+        }
+        // choose
+        int ch = 0, nc = 0;
+        if (freq[i - 1][target.charAt(j - 1) - 'a'] != 0) {
+            ch = (int)(((long)freq[i - 1][target.charAt(j - 1) - 'a'] * f(i - 1, j - 1, target, freq,dp))%MOD);
+        }
+        nc = f(i - 1, j, target, freq,dp);
+        return dp[i][j]=(ch + nc)%MOD;
+
     }
 }
