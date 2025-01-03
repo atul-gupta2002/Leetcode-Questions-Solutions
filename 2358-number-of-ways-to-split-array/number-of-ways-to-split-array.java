25b4222ba110533a9aca09c98ff5386deb713c34
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-01-04 00:23:21 +0530

    Time: 3 ms (59.69%) | Memory: 58.2 MB (73.24%) - LeetSync

--- 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -1,40 +1,22 @@
-// class Solution {
-//     public int waysToSplitArray(int[] nums) {
-//         int sum1=0;
-//         int sum2=0;
-//         int split=0;
-//         for(int i=0;i<nums.length;i++){
-//             sum2+=nums[i];
-//         }
-//         for(int i=0;i<nums.length-1;i++){
-//             sum1 +=nums[i];
-           
-        
-//         if(sum1>=sum2-sum1){
-//             split++;
-//         }
-//         }
-       
-//         return split;
-//     }
-// }
-
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        long leftSum = 0, rightSum = 0;
-        for (int num : nums) {
-            rightSum += num;
+        long sum1=0;
+        long sum2=0;
+        int split=0;
+        for (int num : nums){
+            sum2+=num;
         }
-
-        int validSplits = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            leftSum += nums[i];
-            rightSum -= nums[i];
-            if (leftSum >= rightSum) {
-                validSplits++;
-            }
+        for(int i=0;i<nums.length-1;i++){
+            sum1 +=nums[i];
+               sum2 -=nums[i];
+           
+        
+        if(sum1>=sum2){
+            split++;
         }
-
-        return validSplits;
+        }
+       
+        return split;
     }
 }
+
