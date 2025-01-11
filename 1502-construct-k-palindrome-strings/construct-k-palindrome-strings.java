87d246e8cd3cd460e9a08b8bdf0a1158ba875b30
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-01-12 00:51:03 +0530

    Time: 28 ms (26.26%) | Memory: 45.6 MB (32.37%) - LeetSync

---
+++ 1502-construct-k-palindrome-strings/construct-k-palindrome-strings.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+class Solution {
+    public boolean canConstruct(String s, int k) {
+        if(s.length()<k){
+            return false;
+        }
+        HashMap<Character,Integer> charCount = new HashMap<>();
+        for(char c : s.toCharArray()){
+            charCount.put(c,charCount.getOrDefault(c,0)+1);
+        }
+
+        int oddCount=0;
+        for(int frequency : charCount.values()){
+            if(frequency % 2 != 0){
+                oddCount++;
+            }
+        }
+        return oddCount <=k;
+    }
+}
