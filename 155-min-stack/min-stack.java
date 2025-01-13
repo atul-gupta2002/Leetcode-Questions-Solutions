d77eb7a004a97ea7d6e37352cb0c9cb1bc877fb1
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-01-14 00:18:50 +0530

    Time: 4 ms (97.14%) | Memory: 45.2 MB (24.14%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,27 +1,39 @@
 class MinStack {
     int min = Integer.MAX_VALUE;
-    Stack<Integer> stack = new Stack<Integer>();
-    public void push(int x) {
-        // only push the old minimum value when the current 
-        // minimum value changes after pushing the new value x
-        if(x <= min){          
-            stack.push(min);
-            min=x;
+
+    Stack<Integer> st = new Stack<Integer>();
+    
+    public void push(int val) {
+        if(val<=min){
+            st.push(min);
+            min=val;
         }
-        stack.push(x);
+        st.push(val);    
     }
-
+    
     public void pop() {
-        // if pop operation could result in the changing of the current minimum value, 
-        // pop twice and change the current minimum value to the last minimum value.
-        if(stack.pop() == min) min=stack.pop();
+        if(st.pop()== min){
+            min=st.pop();
+        }
+        
     }
-
+    
     public int top() {
-        return stack.peek();
+       return st.peek();
+        
     }
-
+    
     public int getMin() {
         return min;
+        
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
