a801fe93f20aedd88038cd8f296f8eef30890901
Atul Gupta <109614069+atul-gupta2002@users.noreply.github.com>
2025-07-22 22:17:49 +0530

    Time: 17 ms (33.91%) | Memory: 45.1 MB (97.63%) - LeetSync

---
+++ 594-longest-harmonious-subsequence/longest-harmonious-subsequence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        int result=0;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i : nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        for(int i:map.keySet()){
+            if(map.containsKey(i+1)){
+                result=Math.max(result,map.get(i)+map.get(i+1));
+            }
+        }
+        return result;
+        
+    }
+}
